8a3ddb046d147878c17ac53495419c10302d68aa
thejovialknight <thejovialknight@gmail.com>
2022-07-13 18:46:54 -0700

    Add index, style, and js file

--- rps.js
+++ rps.js
@@ -1,8 +1,55 @@
 function computerPlay() {
-	let options = ['rock', 'paper', 'scissors'];
-	return options[Math.random() * 3];	
+	const options = ['rock', 'paper', 'scissors'];
+	return options[Math.floor(Math.random() * options.length)];	
 }
 
-for(let i = 0; i < 100; i++) {
-	console.log(computerPlay());
+function playRound(playerSelection) {
+	const dominanceMap = new Map([
+		["rock", "scissors"],
+		["scissors", "paper"],
+		["paper", "rock"]
+	]);
+
+	const computerSelection = computerPlay();
+
+	const info = document.querySelector("#status");
+	const playerScore = document.querySelector("#player-score");
+	const computerScore = document.querySelector("#computer-score");
+	console.log("p: " + playerScore.innerHTML);
+	console.log("c: " + computerScore.innerHTML);
+
+	if(playerScore.innerHTML >= 5 || computerScore.innerHTML >= 5) {
+		playerScore.innerHTML = 0;
+		computerScore.innerHTML = 0;
+	}
+
+	if(dominanceMap.get(playerSelection) === computerSelection) {
+		playerScore.innerHTML = parseInt(playerScore.innerHTML) + 1;
+		info.innerHTML = `You win! ${playerSelection} beats ${computerSelection}!`;
+	}
+	else if(dominanceMap.get(computerSelection) === playerSelection) {
+		computerScore.innerHTML = parseInt(computerScore.innerHTML) + 1;
+		info.innerHTML = `You lose! ${computerSelection} beats ${playerSelection}!`;
+	}
+	else {
+		info.innerHTML = `You tie! Player chose ${playerSelection} and computer chose ${computerSelection}!`;
+	}
+
+	if(Number(playerScore.innerHTML) >= 5) {
+		info.innerHTML += " You've won the game! Thanks for playing!"
+	}
+	else if(Number(computerScore.innerHTML) >= 5) {
+		info.innerHTML += " You've lost the game! Thanks for playing!"
+	}
 }
+
+function game() {
+	const info = document.querySelector("#status");
+	info.innerHTML = "Hello, welcome to Rock, Paper, Scissors! Make a selection!"
+
+	document.getElementById("rock").addEventListener("click", () => { playRound("rock"); });
+	document.getElementById("paper").addEventListener("click", () => { playRound("paper"); });
+	document.getElementById("scissors").addEventListener("click", () => { playRound("scissors"); });
+}
+
+game();
